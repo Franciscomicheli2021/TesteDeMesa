322d082e186689e20bf00365532a32e3004c80f5
Francisco Micheli <164116771+Franciscomicheli2021@users.noreply.github.com>
2024-03-24 21:35:18 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  boolean opcao = false;
+  int numero = 1;
+  String texto = "1";
+
+  boolean resultado = (opcao && !opcao);
+
+  numero = (numero + 2);
+
+  resultado = (numero >= 2) || resultado;
+
+  numero = 1;
+
+  reultado = (numero == texto) && resultado;
+  System.out.println(resultado);
+
+  
+}
